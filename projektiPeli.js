5b603f70ee4a9575537abdc6e9f1299dd87add07
nicholasmyllari <nicholas.myllari@aalto.fi>
2017-04-25 15:45:16 +0300

    maailma ja collision

--- projektiPeli.js
+++ projektiPeli.js
@@ -9,7 +9,7 @@ function preload(){
     game.load.image('silde', 'Sprites/slide.png');
     game.load.spritesheet('dude', 'Sprites/run.png', 34, 48);
     game.load.image('sky', 'Sprites/sky.png');
-    game.load.image('ground', 'Sprites/ground');
+    game.load.image('ground', 'Sprites/ground.png');
 
 }
 
@@ -29,13 +29,18 @@ function create() {
 
     platforms = game.add.group();
     platforms.enableBody = true;
+    var ground = platforms.create(0, game.world.height - 64, 'ground');
     ground.scale.setTo(2, 2);
     ground.body.immovable = true;
     var ledge = platforms.create(400, 400, 'ground');
     ledge.body.immovable = true;
-    ledge = platforms.create(-150, 250, 'ground');
+    ledge = platforms.create(0, 250, 'ground');
     ledge.body.immovable = true;
 }
 
 function update() {
+
+    //  Collide the player and the stars with the platforms
+    var hitPlatform = game.physics.arcade.collide(player, platforms);
+
 }
