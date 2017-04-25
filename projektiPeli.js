e4a52f0a0b68d4086d4ec9adfd60389cf31c74fe
nicholasmyllari <nicholas.myllari@aalto.fi>
2017-04-25 13:43:25 +0300

    kuvien lataukset/sprite animaatio
    Molemmat vaiheessa

--- projektiPeli.js
+++ projektiPeli.js
@@ -1,7 +1,15 @@
 <<<<<<< HEAD
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
-function preload() {
+function preload(){
+
+    game.load.image('coal', 'Sprites/sky.png');
+    game.load.image('emerald', 'Sprites/platform.png');
+    game.load.image('jumpUp', 'Sprites/star.png');
+    game.load.image('jumpDown', 'Sprites/star.png');
+    game.load.image('slide', 'Sprites/star.png');
+    game.load.spritesheet('dude', 'Sprites/dude.png', 32, 48);
+
 }
 
 function create() {
