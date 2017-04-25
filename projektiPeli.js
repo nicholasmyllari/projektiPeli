8446541022d99d5f5e233e76b6d62ec9e53d7b3f
nicholasmyllari <nicholas.myllari@aalto.fi>
2017-04-25 13:13:14 +0300

    Revert "korjaus .js tiedostojen kanssa"
    This reverts commit 8967a396425fcb59f2806150503932fd84335b60.

---
+++ projektiPeli.js
@@ -0,0 +1,14 @@
+<<<<<<< HEAD
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+}
+=======
+//Tänne tulee peli
+>>>>>>> origin/master
