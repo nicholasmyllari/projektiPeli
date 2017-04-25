258090e0fc06e2f106f2af6c5665b96a9cc5735b
nicholasmyllari <nicholas.myllari@aalto.fi>
2017-04-25 14:31:22 +0300

    animaatiokokeilu

--- projektiPeli.js
+++ projektiPeli.js
@@ -1,28 +1,27 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload(){
-
-    game.load.image('coal', 'Sprites/CoalSprites.png');
-    game.load.image('gem', 'Sprites/GemSprite.png');
-    game.load.image('jumpUp', 'Sprites/JumpUp.png');
-   // game.load.image('jumpDown', 'Sprites/star.png');
-   // game.load.image('slide', 'Sprites/star.png');
-  //  game.load.spritesheet('dude', 'Sprites/dude.png', 32, 48);
+	game.stage.backgroundColor = "#4488AA";
+    game.load.image('coal', 'Sprites/CoalSprite.jpg');
+    game.load.image('gem', 'Sprites/GemSprite.jpg');
+ 	game.load.image('jumpUp', 'Sprites/JumpUp.jpg');
+    game.load.image('fallDown', 'Sprites/fallDown.png');
+    game.load.spritesheet('dude', 'Sprites/jumpSprite.png', 32, 48);
 
 }
 
 function create() {
 
-	game.add.sprite(0, 0, 'coal');
+	game.add.sprite(10, 0, 'coal');
 
 
-  /*  sprite = game.add.sprite(40, 100, 'coal');
+    sprite = game.add.sprite(40, 100, 'coal');
 
     sprite.animations.add('walk');
 
     sprite.animations.play('walk', 50, true);
 
-    game.add.tween(sprite).to({ x: game.width }, 10000, Phaser.Easing.Linear.None, true);*/
+    game.add.tween(sprite).to({ x: game.width }, 10000, Phaser.Easing.Linear.None, true);
 
 }
 
