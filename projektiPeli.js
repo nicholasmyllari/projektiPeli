677ded12d791f1410b817c2a3e026f0b95ddfa4e
nicholasmyllari <nicholas.myllari@aalto.fi>
2017-04-25 15:22:35 +0300

    animaatio toimii

--- projektiPeli.js
+++ projektiPeli.js
@@ -2,24 +2,23 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create
 
 function preload(){
 	game.stage.backgroundColor = "#4488AA";
-    game.load.image('coal', 'Sprites/CoalSprite.jpg');
-    game.load.image('gem', 'Sprites/GemSprite.jpg');
- 	game.load.image('jumpUp', 'Sprites/JumpUp.jpg');
+    game.load.image('coal', 'Sprites/CoalSprite.png');
+    game.load.image('gem', 'Sprites/gem.png');
+ 	game.load.image('jumpUp', 'Sprites/JumpUp.png');
     game.load.image('fallDown', 'Sprites/fallDown.png');
-    game.load.spritesheet('dude', 'Sprites/jumpSprite.png', 32, 48);
+    game.load.image('silde', 'Sprites/slide.png');
+    game.load.spritesheet('dude', 'Sprites/run.png', 34, 48);
 
 }
 
 function create() {
 
-	game.add.sprite(10, 0, 'coal');
 
-
-    sprite = game.add.sprite(40, 100, 'coal');
+    sprite = game.add.sprite(40, 100, 'dude');
 
     sprite.animations.add('walk');
 
-    sprite.animations.play('walk', 50, true);
+    sprite.animations.play('walk', 5, true);
 
     game.add.tween(sprite).to({ x: game.width }, 10000, Phaser.Easing.Linear.None, true);
 
