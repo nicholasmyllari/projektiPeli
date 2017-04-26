0754f6beac14e3412e4c05606e555d78ca6380b3
nicholasmyllari <nicholas.myllari@aalto.fi>
2017-04-27 00:18:27 +0300

    paljon
    Ground animation, player animation (not done) and spawning of coal and
    gems

--- projektiPeli.js
+++ projektiPeli.js
@@ -1,46 +1,102 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(804, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload(){
 	game.stage.backgroundColor = "#4488AA";
-    game.load.image('coal', 'Sprites/CoalSprite.png');
+    game.load.image('CoalSprite', 'Sprites/CoalSprite.png');
     game.load.image('gem', 'Sprites/gem.png');
  	game.load.image('jumpUp', 'Sprites/JumpUp.png');
     game.load.image('fallDown', 'Sprites/fallDown.png');
     game.load.image('silde', 'Sprites/slide.png');
     game.load.spritesheet('dude', 'Sprites/run.png', 34, 48);
+    game.load.spritesheet('grassyGround', 'Sprites/grassNoWhite.png', 67, 76);
     game.load.image('sky', 'Sprites/sky.png');
     game.load.image('ground', 'Sprites/ground.png');
-
 }
-
+var coals;
+var emeralds;
 var platforms;
 
 function create() {
-
+	//background
     game.add.sprite(0, 0, 'sky');
-
+    //the player and it's physics
     player = game.add.sprite(32, game.world.height - 150, 'dude');
     game.physics.arcade.enable(player);
-    player.body.bounce.y = 0.2;
-    player.body.gravity.y = 300;
+    player.body.gravity.y = 600;
     player.body.collideWorldBounds = true;
-    
-    game.physics.startSystem(Phaser.Physics.ARCADE);
+    player.animations.add('walk');
+    //storage for the coals and emeralds
+    coals = game.add.group();
+	coals.enableBody = true;
+	emeralds = game.add.group();
+	emeralds.enableBody = true;
 
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    //ground that the player walks on
     platforms = game.add.group();
     platforms.enableBody = true;
-    var ground = platforms.create(0, game.world.height - 64, 'ground');
-    ground.scale.setTo(2, 2);
+    var ground = platforms.create(0, game.world.height - 57, 'ground');
     ground.body.immovable = true;
-    var ledge = platforms.create(400, 400, 'ground');
-    ledge.body.immovable = true;
-    ledge = platforms.create(0, 250, 'ground');
-    ledge.body.immovable = true;
+
+    //moving grass on top of the ground that the player runs on
+    for (var i = 12; i >= 0; i--) {
+    grass = game.add.sprite(i * 67, game.world.height - 67, 'grassyGround');
+    grass.scale.x *= -1;
+    grass.animations.add('move');
+    grass.animations.play('move', 6, true);
+    };
 }
+//variables for spawning of coals and emeralds
+var time_til_spawnC = 20000;
+var time_til_spawnE = 20000;
+var last_spawn_timeC = 0;
+var last_spawn_timeE = 0;
+
+
 
 function update() {
 
     //  Collide the player and the stars with the platforms
     var hitPlatform = game.physics.arcade.collide(player, platforms);
+    //animation for the player
+	if (player.body.velocity.y < 0) {
+		player.animations.stop();
+    } else if (player.body.velocity.y > 0) {
+    	player.animations.stop();
+    } else {
+    	player.animations.play('walk', 20, true);
+    }
 
+    //controls
+    cursors = game.input.keyboard.createCursorKeys();
+    if (cursors.down.isDown && player.body.touching.down && hitPlatform) { //slide
+        player.animations.stop();
+    }
+    else if (cursors.up.isDown && player.body.touching.down && hitPlatform) { //jump
+    	player.animations.stop();
+        player.body.velocity.y = -350;
+    }
+    //spawn enemies
+  var current_time = game.time.time;
+  if(current_time - last_spawn_timeC > time_til_spawnC) {
+    time_til_spawnC = Math.random()*3000 + 2000;
+    last_spawn_timeC = current_time;
+    spawnCoal();
+  }
+  if(current_time - last_spawn_timeE > time_til_spawnE) {
+    time_til_spawnE = Math.random()*8000 + 6000;
+    last_spawn_timeE = current_time;
+    spawnEmerald();
+  }
 }
+
+function spawnCoal() {
+ 	var coal = coals.create(700, 400, 'CoalSprite')
+ 	coal.body.velocity.x = -150;
+}
+
+function spawnEmerald() {
+	var emerald = emeralds.create(700, 200, 'gem')
+	emerald.body.velocity.x = -150;
+}
+
