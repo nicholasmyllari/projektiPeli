fc25c0bd80d3fc392e58fcd729556e083f73fa51
nicholasmyllari <nicholas.myllari@aalto.fi>
2017-05-10 01:07:16 +0300

    jumpUp

--- preload.js
+++ preload.js
@@ -2,9 +2,8 @@ var preloadState = {
     preload: function () {
     game.load.image('CoalSprite', 'Sprites/CoalSprite.png');
     game.load.image('gem', 'Sprites/gem.png');
- 	game.load.spritesheet('jumpUp', 'Sprites/JumpUp.png', 33, 50);
+ 	game.load.spritesheet('jumpUp', 'Sprites/jumpUp.png', 33, 50);
     game.load.spritesheet('fallDown', 'Sprites/fallDown.png', 35, 47);
-    game.load.spritesheet('slide', 'Sprites/slide.png', 45, 37);
     game.load.spritesheet('dude', 'Sprites/run.png', 34, 48);
     game.load.spritesheet('grassyGround', 'Sprites/grassAnimation.png', 67, 76);
     game.load.image('sky', 'Sprites/sky.png');
